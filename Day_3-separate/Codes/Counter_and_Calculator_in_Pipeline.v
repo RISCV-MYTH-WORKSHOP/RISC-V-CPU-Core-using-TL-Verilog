962740dffb20e3a919a2aa3680845295f23b5243
Redbeard358 <ashutosh30.sahoo@gmail.com>
2020-08-29 15:36:41 +0530

    Added Counter and Calculator in pipeline

--- Day_3-separate/Codes/Counter_and_Calculator_in_Pipeline.v
+++ Day_3-separate/Codes/Counter_and_Calculator_in_Pipeline.v
@@ -12,18 +12,24 @@
    m4_makerchip_module   // (Expanded in Nav-TLV pane.)
 \TLV
    $reset = *reset;
-
-   |comp
-      
+   
+   |calc
       @1
-         $err1 = $bad_input || $illegal_op;
-      
-      @3
-         $err2 = $err1 || $over_flow;
-      
-      @5
-         $err3 = $err2 || $div_by_zero;  
+         $reset = *reset;
          
+      @1
+         $val1 [31:0] = >>1$out [31:0];
+         $val2 [31:0] = $rand2[3:0];
+   
+         $sum [31:0] = $val1 + $val2;
+         $diff[31:0] = $val1 - $val2;
+         $prod[31:0] = $val1 * $val2;
+         $quot[31:0] = $val1 / $val2;
+         
+         $out [31:0] = ($reset ? 0 : ($op[0] ? $sum : ($op[1] ? $diff : ($op[2] ? $prod : $quot )))) ;
+         
+         $cnt [31:0] = $reset ? 0 : >>1$cnt + 1 ;
+                 
    // Assert these to end simulation (before Makerchip cycle limit).
    *passed = *cyc_cnt > 40;
    *failed = 1'b0;

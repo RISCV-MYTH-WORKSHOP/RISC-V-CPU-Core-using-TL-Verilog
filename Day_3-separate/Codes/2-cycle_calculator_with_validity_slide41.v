618971b4764df33df282db82639c8e939a3a7152
Redbeard358 <33366552+Redbeard358@users.noreply.github.com>
2020-09-06 19:48:08 +0530

    Slide No - 41 - Corrected the code as per reference diagram
    Corrected the 2-cycle calculator code with validity code as per the reference diagram.  Changed $valid | $reset; to $valid | | $reset;  . $valid should be 1 bit.  No need of $valid || $reset in the $out expression.

--- Day_3-separate/Codes/2-cycle_calculator_with_validity_slide41.v
+++ Day_3-separate/Codes/2-cycle_calculator_with_validity_slide41.v
@@ -1,18 +1,13 @@
 \m4_TLV_version 1d: tl-x.org
 \SV
-
-   // =========================================
-   // Welcome!  Try the tutorials via the menu.
-   // =========================================
-
-   // Default Makerchip TL-Verilog Code Template
+   // This code can be found in: https://github.com/stevehoover/RISC-V_MYTH_Workshop
    
-   // Macro providing required top-level module definition, random
-   // stimulus support, and Verilator config.
+   m4_include_lib(['https://raw.githubusercontent.com/stevehoover/RISC-V_MYTH_Workshop/bd1f186fde018ff9e3fd80597b7397a1c862cf15/tlv_lib/calculator_shell_lib.tlv'])
+
+\SV
    m4_makerchip_module   // (Expanded in Nav-TLV pane.)
-\TLV
-   //$reset = *reset;
-   
+
+\TLV   
    |calc
       @0
          $reset = *reset;
@@ -21,9 +16,8 @@
          $val1 [31:0] = >>2$out [31:0];
          $val2 [31:0] = $rand2[3:0];
          
-         $valid [31:0] = $reset ? 32'b0 : >>1$valid + 1 ;
-         $valid_or_reset = $valid | $reset;
-   
+         $valid [31:0] = $reset ? 1'b0 : >>1$valid + 1'b1 ;
+         $valid_or_reset = $valid || $reset;
          
       ?$vaild_or_reset
          @1   
@@ -32,14 +26,31 @@
             $prod[31:0] = $val1 * $val2;
             $quot[31:0] = $val1 / $val2;
             
-         @2                                           
-            $out [31:0] = $reset ? 32'b0 : 
-                          $valid_or_reset ? ($op[0] ? $sum : ($op[1] ? $diff : ($op[2] ? $prod : $quot ))) :
-                          >>2$out;
+         @2   
+            $out [31:0] = $reset ? 32'b0 :
+                          ($op[1:0] == 2'b00) ? $sum :
+                          ($op[1:0] == 2'b01) ? $diff :
+                          ($op[1:0] == 2'b10) ? $prod :
+                                                $quot ;
             
-                 
+            
+
+      // Macro instantiations for calculator visualization(disabled by default).
+      // Uncomment to enable visualisation, and also,
+      // NOTE: If visualization is enabled, $op must be defined to the proper width using the expression below.
+      //       (Any signals other than $rand1, $rand2 that are not explicitly assigned will result in strange errors.)
+      //       You can, however, safely use these specific random signals as described in the videos:
+      //  o $rand1[3:0]
+      //  o $rand2[3:0]
+      //  o $op[x:0]
+      
+   //m4+cal_viz(@3) // Arg: Pipeline stage represented by viz, should be atleast equal to last stage of CALCULATOR logic.
+
+   
    // Assert these to end simulation (before Makerchip cycle limit).
    *passed = *cyc_cnt > 40;
    *failed = 1'b0;
+   
+
 \SV
    endmodule

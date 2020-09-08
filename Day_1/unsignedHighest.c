e24677c9f1fc20bbc679c948f82c96113af7151f
Redbeard358 <ashutosh30.sahoo@gmail.com>
2020-09-08 15:35:51 +0530

    Shifted all files to the codes folder

--- Day_1/unsignedHighest.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int main()
-{
-	unsigned long long int max = (unsigned long long int) (pow(2,64) - 1);
-	printf("The highest number that can be represented by unsigned long lomg int is: %llu\n", max);
-	return 0;
-}
